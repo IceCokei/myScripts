1a15f786eacdbf67a6568d050ae718cb70386f20
Coke <104893663+IceCokei@users.noreply.github.com>
2025-04-29 13:37:02 +0800

    Delete BackUp/github.js

--- BackUp/github.js
+++
@@ -1,9 +0,0 @@
-#!name=Github 429
-#!desc=解除 Github 429 限制
-
-
-[Header Rewrite]
-http-request (raw|gist).githubusercontent.com header-replace Accept-Language en-us
-
-[MITM]
-hostname = %APPEND% raw.githubusercontent.com,gist.githubusercontent.com
